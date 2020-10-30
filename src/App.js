a72f6791c11007052434e9f88f8927f81ceacf58
Ekin Karadag <6624768+ekinkaradag@users.noreply.github.com>
2020-10-30 20:10:55 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi to <code>React</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://ekinkaradag.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit my blog
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
