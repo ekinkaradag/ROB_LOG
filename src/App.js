f783fef1dc5958d4291e3b07419749973b14d922
Ekin Karadag <6624768+ekinkaradag@users.noreply.github.com>
2020-10-31 17:59:44 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -6,33 +6,26 @@ class App extends Component{
   constructor(){
     super();
     this.state = {
-      string: 'Hi to React'
+      monsters: [
+        {
+          name: 'Frankie'
+        },
+        {
+          name: 'Dracula'
+        },
+        {
+          name: 'Zombie'
+        }
+      ]
     }
   }
 
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={()=>this.setState({string: 'Work in progress'})}> More info</button>
-          <a
-            className="App-link"
-            href="https://ekinkaradag.github.io"
-            rel="noopener noreferrer"
-          >
-            Visit my GitHub Pages
-          </a>
-          <a
-            className="App-link"
-            href="https://ekinkaradag.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Visit my blog
-          </a>
-        </header>
+        {
+            this.state.monsters.map(monsters => <h1>{ monsters.name }</h1>)
+        }
       </div>
     );
     }
